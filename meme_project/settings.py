01d76647c605259d539c6dcff8ae4b7d1af092d7
NaveAi <p8699322@gmail.com>
2024-09-12 10:39:15 +0300

    Update settings.py

--- meme_project/settings.py
+++ meme_project/settings.py
@@ -37,6 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'meme_project.urls'
