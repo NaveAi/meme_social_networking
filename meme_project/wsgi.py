19b24a0cb0bdb3be79b758239dfea85d787ca286
NaveAi <p8699322@gmail.com>
2024-09-16 01:52:02 +0300

    Update wsgi.py

--- meme_project/wsgi.py
+++ meme_project/wsgi.py
@@ -1,9 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meme_project.settings')
 application = get_wsgi_application()
 
-if os.getenv('MIGRATE_ON_START_INPUT', 'false').lower() == 'true':
-    call_command('migrate')
