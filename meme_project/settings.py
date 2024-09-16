77cf52350142d4fd68e328752070e4c7e9d91c7d
NaveAi <p8699322@gmail.com>
2024-09-16 10:54:47 +0300

    Update settings.py

--- meme_project/settings.py
+++ meme_project/settings.py
@@ -93,7 +93,7 @@ AUTH_PASSWORD_VALIDATORS = [
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = 'eu-north-1'  # רק שם האזור
+AWS_S3_REGION_NAME = 'us-west-2'  # רק שם האזור
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
